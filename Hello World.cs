c0edd435c1efb3b6fdc43dfbbdff334568329746
SazzerC <76841020+sarah-christie@users.noreply.github.com>
2021-03-11 17:33:54 +0000

    Add files via upload

---
+++ Hello World.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
